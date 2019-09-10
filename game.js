fc16bfc1036e291b23f0c5962f0b79a6446ea6a4
Casper Henningsson <cabbe.h01@gmail.com>
2019-09-10 15:12:24 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 800,
     parent: 'game',
     backgroundColor: 0x0ecf8f,
     scene: {
@@ -13,7 +13,7 @@ var config = {
         default: "matter",
         matter: {
             gravity: {
-                y: 0
+                y: 1
             }
             //debug: true
         }
@@ -31,16 +31,26 @@ function preload ()
 
 function create ()
 {
-    car = this.matter.add.image(100, 450, 'car');
+    car = this.matter.add.image(100, 150, 'car');
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update ()
 {
-    if(cursors.down.JustDown){
+    if(cursors.down.isDown){
         car.setVelocityY(10);
     }
-    else if(){
-        
+    else if(cursors.up.isDown){
+        car.setVelocityY(-10);
+    }
+    else if(cursors.left.isDown){
+        car.setVelocityX(-10);
+    }
+    else if(cursors.right.isDown){
+        car.setVelocityX(10);
+    }
+    else if(cursors.space.isDown){
+        car.setVelocityY(0);
+        car.setVelocityX(0);
     }
 }
