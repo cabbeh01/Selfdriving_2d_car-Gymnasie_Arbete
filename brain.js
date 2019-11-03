1ea59eaaa20d77b52cf076689c2b8ea659a29681
Cabbeh01 <cabbe.h01@gmail.com>
2019-11-03 20:07:15 +0100

    On my way

--- brain.js
+++ brain.js
@@ -1,1 +1,16 @@
 //In this file I'm going to start develope the neural network for the car
+
+/*
+
+Sen1-
+     \
+      ---  Hidden Layer --
+                          \
+     /                     \
+Sen2-     |Hidden Layer|    ---- Output
+     \                     /     * Steer Right
+      ---  Hidden Layer --/      * Steer Left
+     /                           * Move forward
+Sen3-
+
+*/
